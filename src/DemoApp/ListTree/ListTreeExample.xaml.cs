cfb6092ac9d44d1d230f5671ea2f099a89643aa0
wgross <github@text-mode.de>
2024-02-12 20:28:58 +0100

    [REMOVED] Listree: not working

---
+++ src/DemoApp/ListTree/ListTreeExample.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace DemoApp.ListTree;
+
+public partial class ListTreeExample : UserControl
+{
+    public ListTreeExample()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ src/DemoApp/ListTree/ListTreeExampleViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace DemoApp.ListTree;
+
+public class LevelItemViewModel : ObservableObject
+{
+    public string Name { get; init; }
+}
+
+
+public class LevelViewModel : ObservableObject
+{
+    public string Name { get; init; }
+
+    public int Index { get; init; }
+
+    public ObservableCollection<LevelItemViewModel> Items { get; } = new ObservableCollection<LevelItemViewModel>()
+    {
+        new LevelItemViewModel{Name="one"},
+
+        new LevelItemViewModel{Name="two" }
+    };
+}
+
+public class ListTreeExampleViewModel : ObservableObject
+{
+    public ObservableCollection<LevelViewModel> Levels { get; } = new ObservableCollection<LevelViewModel>()
+    {
+        new LevelViewModel{Name="one", Index=0 },
+
+        new LevelViewModel{Name="two", Index=1 }
+    };
+}
